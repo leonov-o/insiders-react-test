3d7c076fcaa5f3c50dc243fb5687117da750ad90
leonov-o <oleksandr.leonov.work@gmail.com>
2025-01-21 21:20:56 +0200

    Setting up routing in the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import {UsersPage} from "./pages/UsersPage.tsx";
+import {EditUserPage} from "./pages/EditUserPage.tsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<UsersPage/>}/>
+                <Route path="/edit" element={<EditUserPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Button} from "./shared";
+import {NavLink} from "react-router";
 
 export const Header = () => {
     return (
         <div className="flex h-20 items-center justify-center space-x-5 border-b border-black">
-            <Button className="w-52">Edit Users</Button>
-            <Button className="w-52">Users</Button>
+            <NavLink to="/edit" className={({isActive}) => (isActive ? "bg-primary" : "")}>
+                <Button className="w-52">Edit Users</Button>
+            </NavLink>
+            <NavLink to="/" className={({isActive}) => (isActive ? "bg-primary" : "")}>
+                <Button className="w-52">Users</Button>
+            </NavLink>
+
         </div>
     );
 };
