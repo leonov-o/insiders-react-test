d3a5d7bae05f83fc61e493c96bbcf003451fb15a
leonov-o <oleksandr.leonov.work@gmail.com>
2025-01-21 16:33:03 +0200

    Checkbox fix.

--- src/components/shared/Checkbox.tsx
+++ src/components/shared/Checkbox.tsx
@@ -4,16 +4,14 @@ import {CheckIcon} from "@radix-ui/react-icons";
 
 interface CheckboxProps {
     checked: boolean;
-    onChange: () => void;
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({checked, onChange}) => {
+export const Checkbox: React.FC<CheckboxProps> = ({checked}) => {
     return (
         <div className={cn(
             "h-6 w-6 border-black border flex items-center justify-center",
             {"bg-black": checked}
         )}
-             onClick={onChange}
         >
             {checked ? <CheckIcon className="size-5 text-white"/> : null}
         </div>
