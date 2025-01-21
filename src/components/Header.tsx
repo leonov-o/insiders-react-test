f1f65a45e0cb8a583dda3b4f6d81b8fca83fb37f
leonov-o <oleksandr.leonov.work@gmail.com>
2025-01-21 12:37:21 +0200

    Header border fix.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Button} from "./shared";
 
 export const Header = () => {
     return (
-        <div className="flex h-20 items-center justify-center space-x-5 border border-black">
+        <div className="flex h-20 items-center justify-center space-x-5 border-b border-black">
             <Button className="w-52">Edit Users</Button>
             <Button className="w-52">Users</Button>
         </div>
